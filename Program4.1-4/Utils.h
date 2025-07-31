7236a140479a6e5b0012240174a811c2e1fc7c48
Exequiel Pagliari <exepa@hotmail.com>
2025-07-31 11:25:24 -0300

    AddProgram4.1-4

---
+++ Program4.1-4/Utils.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Utils
+{
+
+public:
+	static string readShaderSource(const char* filePath);
+	static void printShaderLog(GLuint shader);
+	static void printProgramLog(int prog);
+	static bool checkOpenGLError();
+	static GLuint createShaderProgram(const char* vp, const char* fp);
+
+};
